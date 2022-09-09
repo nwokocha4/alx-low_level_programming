8a59b0355e508d82ecf5a948433e3b0664d9e4cc
Nwokocha Emeka <nwokochaemeka4@gmail.com>
2022-09-08 17:54:12 -0700

    t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-
 /**
 * main - print the string in the put function
 * Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle 
+* this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
