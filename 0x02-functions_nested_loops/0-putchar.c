e88ee7909273e9df7eda304dad5d0f7d861311e1
Nwokocha Emeka <nwokochaemeka4@gmail.com>
2022-09-14 17:18:15 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,13 +6,12 @@
  */
 int main(void)
 {
-char s[] = "_putchar";
-int i = 0;
-while (s[i] != '\0')
+int i;
+char p[] = "_putchar";
+for (i= 0; i< 8 ; i++)
 {
-_putchar(s[i]);
-i++;
+_putchar(p[i]);
 }
-_putchar(10);
+_putchar('\n');
 return (0);
 }
