a6a8fc126728453e762a8daf0f29282ce884eb62
Nwokocha Emeka <nwokochaemeka4@gmail.com>
2022-09-14 20:38:54 -0700

    task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,8 +4,8 @@
  * * main - main block
  * Description: Find and print the first 98 fib numbers starting with 1 and 2
  * * Numbers should be coma and space separated.
- * * Return: 0 */
- 
+ * * Return: 0
+ */
 int main(void)
 {
 unsigned long int fr1 = 0, bk1 = 1, fr2 = 0, bk2 = 2;
