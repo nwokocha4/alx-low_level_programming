553ac9ada834f0c5010d39e62a856991ebec535d
Nwokocha Emeka <nwokochaemeka4@gmail.com>
2022-09-15 14:51:01 -0700

    task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,4 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
  *  * main - Entry point
  *   *
@@ -8,14 +6,8 @@
  */
 int main(void)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else
-printf("%d is negative\n", n);
-return (0);
+int i;
+i= 0;
+positive_or_negative(i);
+return (0)
 }
