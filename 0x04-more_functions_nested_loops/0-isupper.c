d5321bb5eaae74f122886a816fa521fd0be42294
Nwokocha Emeka <nwokochaemeka4@gmail.com>
2022-09-16 13:17:56 -0700

    task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,21 +1,18 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
+
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ *  * _isupper - checks uppercase
+ *   * @c: parameter
+ *    * Return: 0 if sucessful
  */
-int main(void)
+int _isupper(int c)
 {
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-if (n > 0)
-printf("%d is positive\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
+if (c >= 65 && c < 91)
+{
+return (1);
+}
 else
-printf("%d is negative\n", n);
+{
 return (0);
+}
 }
